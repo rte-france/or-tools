e2a0679b2808b3d3c86e0469f9e9adc38c36caef
Peter Mitri <peter.mitri@rte-france.com>
2025-05-15 14:32:12 +0200

    revert last commit

--- ortools/xpress/environment.cc
+++ ortools/xpress/environment.cc
@@ -298,7 +298,6 @@ bool initXpressEnv(bool verbose, int xpress_oem_license_key) {
         LOG(WARNING) << "Optimizer version: " << version
                      << " (OR-Tools was compiled with version " << XPVERSION
                      << ").\n";
-        LOG(WARNING) << "THIS IS A SNAPSHOT VERSION\n";
       }
       return true;
     } else {
